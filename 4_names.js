dea3c500a4fd4a7f13af02b61a8b58192d3e70c3
up1512001 <utsav.patel1512@outlook.com>
2022-09-03 11:01:09 +0530

    modules completed

---
+++ 4_names.js
@@ -0,0 +1,9 @@
+// local name
+const name = "secrect name"
+
+// global name
+
+const utsav="utsav"
+const patel = "patel"
+
+module.exports = {utsav,patel};
---
+++ app.js
@@ -0,0 +1,13 @@
+const name = require('./4_names')
+const hi = require('./5_utils')
+
+const data = require('./6_alternative_flavor')
+
+// console.log(data);
+
+// console.log(name,hi);
+
+// hi(name.utsav)
+// hi(name.patel)
+
+require('./7_mind_grenade');
