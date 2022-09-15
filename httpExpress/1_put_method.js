7f53f0bbe3769993e5e1e0bd7297c5ddac7e0301
up1512001 <utsav.patel1512@outlook.com>
2022-09-15 15:43:00 +0530

    left commit

---
+++ httpExpress/1_put_method.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+let {people} = require('./data');
+
+app.use(express.static('./methods-public'))
+
+app.use(express.urlencoded({extended:false}))
+
+
+app.use(express.json()); 
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({success:true,data:people})
+})
+
+app.post('/api/people',(req,res)=>{
+    const {name} = req.body;
+    if(!name){
+        return res.status(400).json({success:false,msg:'enter name value'})
+    }
+    res.status(201).json({success:true,person:name});
+})
+
+app.post('/login',(req,res)=>{
+    // console.log(req.body);
+    const {name} = req.body
+    if(name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Enter name')
+})
+
+
+
+app.put('/api/people/:id',(req,res)=>{
+    const {id} = req.params;
+    const {name} = req.body;
+
+    // console.log(id,name);
+    // res.send(`${id} ${name}`)
+
+    const person = people.find((person=>person.id === Number(id)))
+    if(!person){
+        return res.status(404).json({success:false,msg:`no person with id ${id}...`})
+    }
+
+    const newPeople = people.map((person)=>{
+        if(person.id === Number(id)){
+            person.name = name;
+        }
+        return person;
+    })
+
+    res.status(200).json({success:true,data:newPeople})
+})
+
+
+app.listen(5000,()=>{
+    console.log('5000 listning...');
+})
